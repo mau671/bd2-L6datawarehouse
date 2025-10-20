2217e647239db4b610653dd31ce5711ab8ec483b
Ximena Molina <ximenamolinaportilla01@gmail.com>
2025-10-20 16:38:17 -0600

    Update sales dates to 2024 in SQL script

--- ActualizaciónFechas.sql
+++ ActualizaciónFechas.sql
@@ -1,10 +1,11 @@
 --Actualice la data para que las ventas
---queden en 2024 (campo DocDate de OINV y ORIN, est·n a 2020,
---sumar los 4 aÒos)
+--queden en 2024 (campo DocDate de OINV y ORIN, est√°n a 2020,
+--sumar los 4 a√±os)
 
 UPDATE DBO.OINV
-SET DocDate = DATEADD(yyyy,5,DocDate)
+SET DocDate = DATEADD(yyyy,4,DocDate)
 
 UPDATE DBO.ORIN
-SET DocDate = DATEADD(yyyy,5,DocDate)
+SET DocDate = DATEADD(yyyy,4,DocDate)
+
 
