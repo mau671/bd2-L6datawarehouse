41ceea7c9ce2791d6dfe494bba8e2fe667cef8d7
Ximena Molina <ximenamolinaportilla01@gmail.com>
2025-10-20 16:32:50 -0600

    QueryActualizarFechas

---
+++ ActualizaciónFechas.sql
@@ -0,0 +1,10 @@
+--Actualice la data para que las ventas
+--queden en 2024 (campo DocDate de OINV y ORIN, están a 2020,
+--sumar los 4 años)
+
+UPDATE DBO.OINV
+SET DocDate = DATEADD(yyyy,5,DocDate)
+
+UPDATE DBO.ORIN
+SET DocDate = DATEADD(yyyy,5,DocDate)
+
